ec85d382be59b28ee9490af68f355bced6fa29cd
guibs <guilherme.barbosa@germinare.org.br>
2024-08-30 08:17:02 -0300

    adicionando sh

--- RPACotacao.py
+++ RPACotacao.py
@@ -4,8 +4,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from datetime import datetime
 import time
-import os
 from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("-headless")  # Executar o Chrome em modo headless (sem janela visível)
@@ -15,31 +17,30 @@ time.sleep(2)
 
 data_hora = datetime.now() 
 data = data_hora.strftime("%Y-%m-%d")
-print(data)
 hora = data_hora.strftime("%H:%M:%S")
-print(hora)
 
-print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
-
-cotacao_dolar = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/section[1]/div[1]/a/div/span[2]').text
+cotacao_dolar = driver.find_element(By.XPATH, '/html/body/div[3]/article/div[2]/div/div[1]/div/div/div[3]/section/div[1]/div/div[2]/div/input').get_attribute('value')
 cotacao_dolar = float (cotacao_dolar.replace(',', '.'))
-print(cotacao_dolar)
 
-load_dotenv()
-connection = None
+connection=None
 try:
     connection = psycopg2.connect(user=os.getenv("USER"),
                                     password=os.getenv("PASSWORD"),
                                     host=os.getenv("HOST"),
                                     port=os.getenv("PORT"),
                                     database=os.getenv("DATABASE"))
+   
+    print("conexão estabelecida com sucesso!")
     cursor = connection.cursor()
-
+    
+    print((data, hora, cotacao_dolar))
     cursor.execute('CALL INSERIR_VALOR_DOLAR(%s, %s, %s)', (data, hora, cotacao_dolar))
     connection.commit()
     print("Dados inseridos com sucesso!")
+
 except (Exception, Error) as error:
     print("Erro ao inserir os dados:", error)
+
 finally:
     if connection:
         cursor.close()
